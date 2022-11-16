359c9e6f0c5b41d0aad17f45dbda3463aaa25a90
* <hef666@gmail.com>
2022-11-16 17:07:50 -0600

    hola

---
+++ modulos/fubciones_creadas.py
@@ -0,0 +1,5 @@
+import operaciones as op
+print("la suna es ",op.suma(5,6))
+print("la resta es ",op.resta(5,6))
+print("la multiplicacion es ",op.mult(7,5))
+print("la divicion es ",op.divide(4,2))
